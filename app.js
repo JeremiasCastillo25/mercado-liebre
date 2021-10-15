063b6cf063affbe5d1d970519a8b8bffec0aa110
LuiosDyet <luisdartayet@gmail.com>
2021-10-14 21:42:55 -0300

    arreglos varios en el HTML principalmente

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
     console.log("Server en puerto 3000");
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/home.html"));
+    res.sendFile(path.join(__dirname, "views/home.html"));
 });
