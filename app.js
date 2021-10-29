09b26723990dc9b2ce1749ed5c023e1ee4d50edc
LuiosDyet <luisdartayet@gmail.com>
2021-10-29 19:44:18 -0300

    M04C06

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
 });
+
+app.get("/product-creation", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/product-creation.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
